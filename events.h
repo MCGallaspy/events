478f907ceec28d3cbe51f46be3d2fb2e7b64719a
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-07-17 23:27:20 -0700

    Avoid undefined static_cast
    Using boost::hana!
    
    However it only works if you have a compile-time
    list of event classes. One could do this:
    1. By requiring the programmer to list each one.
       Potentially error prone -- can it be robust if a class
       is accidentally excluded?
    2. Perhaps with some more template metamagic? But it's
       hard to figure out...

--- events.h
+++ events.h
@@ -1,3 +1,4 @@
+#include <boost/hana.hpp>
 #include <memory>
 #include <type_traits>
 #include <vector>
@@ -36,22 +37,30 @@ public:
 template<class Event, class... Others>
 class EventListener<Event, Others...> : public EventListener<Event>, public EventListener<Others...> {};
 
+typedef EventListener<>* EventListenerPtr;
+
+// Essentially a variable template
+template<typename ...>
+struct ListenerHelper {
+  typedef std::vector<EventListenerPtr> Vec;
+  typedef std::unique_ptr<Vec> Ptr;
+  static Ptr listeners;
+};
+
+
+/*
+It sort of works now... but we need a way to create event_types below at compile time.
+*/
+class AEvent;
+class BEvent;
+class DEvent;
+namespace hana = boost::hana;
+auto event_types = hana::make_tuple(hana::type_c<AEvent>, hana::type_c<BEvent>, hana::type_c<DEvent>);
+
 /* EventDispatcher avoids using RTTI by using templated singleton helper classes.
  * These basically amount to c++14's variable templates.
 */
 class EventDispatcher {
-private:
-  typedef EventListener<>* EventListenerPtr;
-
-  // Essentially a variable template
-  template<class Event>
-  class ListenerHelper {
-  public:
-    typedef std::vector<EventListenerPtr> Vec;
-    typedef std::unique_ptr<Vec> Ptr;
-    static Ptr listeners;
-  };
-
 public:
   static const EventDispatcher& getInstance() {
     static EventDispatcher ed;
@@ -63,6 +72,14 @@ public:
     for (auto listener : (*ListenerHelper<Event>::listeners)) {
       static_cast<EventListener<Event>*>(listener)->onEvent(evt);
     }
+    constexpr auto pred = hana::reverse_partial(hana::traits::is_base_of, hana::type_c<Event>);
+    auto filtered = hana::filter(event_types, pred);
+    hana::for_each(filtered, [&](auto Base) {
+      using BaseT = typename decltype(Base)::type;
+      for (auto listener : (*ListenerHelper<Event, BaseT>::listeners)) {
+        static_cast<EventListener<BaseT>*>(listener)->onEvent(static_cast<BaseT>(evt));
+      }
+    });
   }
 
   template<class Event>
@@ -74,7 +91,7 @@ public:
   void connectHandler(EventListener<Base>* el) {
     static_assert(!std::is_same<Derived, Base>::value, "Can't connect event handler to itself.");
     static_assert(std::is_base_of<Base, Derived>::value, "The 'Derived' event must be derived from the 'Base' event");
-    ListenerHelper<Derived>::listeners->push_back(static_cast<EventListenerPtr>(el));
+    ListenerHelper<Derived, Base>::listeners->push_back(static_cast<EventListenerPtr>(el));
   }
 
 private:
@@ -83,5 +100,5 @@ private:
 
 // ZOMG
 // This horrible mess is the static variable definition for ListenerHelper.
-template<class Event>
-typename EventDispatcher::ListenerHelper<Event>::Ptr EventDispatcher::ListenerHelper<Event>::listeners = typename EventDispatcher::ListenerHelper<Event>::Ptr(new typename EventDispatcher::ListenerHelper<Event>::Vec);
+template<typename ... Pack>
+typename ListenerHelper<Pack...>::Ptr ListenerHelper<Pack...>::listeners = typename ListenerHelper<Pack...>::Ptr(new typename ListenerHelper<Pack...>::Vec);
